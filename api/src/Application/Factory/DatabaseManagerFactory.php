bb17396abe52fb832a53227c1eeefb77dfbfb33d
Saul Diaz <saul.diaz.v@gmail.com>
2024-10-16 13:07:04 -0600

    Feature: Database definition.

--- api/src/Application/Factory/DatabaseManagerFactory.php
+++ api/src/Application/Factory/DatabaseManagerFactory.php
@@ -14,19 +14,7 @@ class DatabaseManagerFactory
 
     public function __construct()
     {
-         // this would usually be in your dependency settings or on a safe place within 
-         // your application. This is purely for explanatory reasons that is has been placed here
          $dbSettings = [
-            /*
-            'driver' => 'mysql',
-            'host' => 'localhost',
-            'database' => 'gateway',
-            'username' => 'db_username',
-            'password' => 'db_password',
-            'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
-            'prefix' => ''
-            */
             'driver' => 'sqlite',
             'database' => __DIR__ . '/../../Db/database.sqlite'
         ];
