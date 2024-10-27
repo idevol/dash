d7cad2defa7c3e9ffdee27a1ee86a505d5ae447f
Saul Diaz <saul.diaz.v@gmail.com>
2024-10-27 10:27:12 -0600

    Feature: Setup VITE_DASH_HOST in 'web/index.html'

--- lando/scripts/04-setup-env-files.sh
+++ lando/scripts/04-setup-env-files.sh
@@ -11,6 +11,7 @@ WEB_ENV_FILE=/app/web/.env
 
 LANDO_URL=`php /app/lando/scripts/get-lando-url.php`
 DASH_HOST_ENV="DASH_HOST=${LANDO_URL}";
+VITE_DASH_HOST_ENV="VITE_DASH_HOST=${LANDO_URL}";
 echo "🚀 LANDO URL: ${GREEN}${LANDO_URL}${NC}\n"
 
 if test -f "${API_ENV_FILE}"; then
@@ -32,16 +33,16 @@ fi
 
 if test -f "${WEB_ENV_FILE}"; then
   cd /app/web
-  if grep -q "${DASH_HOST_ENV}" "${WEB_ENV_FILE}"; then
-    echo "✅ Declared \"${DASH_HOST_ENV}\" in \"web/.env\" file."
+  if grep -q "${VITE_DASH_HOST_ENV}" "${WEB_ENV_FILE}"; then
+    echo "✅ Declared \"${VITE_DASH_HOST_ENV}\" in \"web/.env\" file."
   else
-    if grep -q "DASH_HOST" "${WEB_ENV_FILE}"; then
-      WEB_ENV_FILE_CONTENT=`while read -r line; do if echo "$line" | grep -q DASH_HOST; then echo "${DASH_HOST_ENV}"; else echo "$line"; fi; done < .env`
+    if grep -q "VITE_DASH_HOST" "${WEB_ENV_FILE}"; then
+      WEB_ENV_FILE_CONTENT=`while read -r line; do if echo "$line" | grep -q DASH_HOST; then echo "${VITE_DASH_HOST_ENV}"; else echo "$line"; fi; done < .env`
       echo "${WEB_ENV_FILE_CONTENT}" > "${WEB_ENV_FILE}"
     else
-      echo "\n${DASH_HOST_ENV}" >> "${WEB_ENV_FILE}"
+      echo "\n${VITE_DASH_HOST_ENV}" >> "${WEB_ENV_FILE}"
     fi
-    echo "✅ Setup \"${DASH_HOST_ENV}\" in \"web/.env\" file."
+    echo "✅ Setup \"${VITE_DASH_HOST_ENV}\" in \"web/.env\" file."
   fi
 else
   echo '❌ File "web/.env" not found.'
